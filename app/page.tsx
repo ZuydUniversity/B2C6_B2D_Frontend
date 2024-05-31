9a7b52cce29a28cb7d81de31b83c2f98e810e396
R-ZPG <2104900servais@zuyd.nl>
2024-05-31 14:14:55 +0200

    fixed Test

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import Layout from './layout';
 
-const HomePage = () => {
+export default function Home() {
     return (
-        <Layout>
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
                 <p className="top-300 text-3xl text-center">
                     Hello World!
                 </p>
             </div>
-        </Layout>
     );
-};
-
-export default HomePage;
+};npm
