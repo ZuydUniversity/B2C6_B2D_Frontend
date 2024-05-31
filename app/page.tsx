305a0e3ddaa777d9e55cf125ac75adf190f8745d
R-ZPG <2104900servais@zuyd.nl>
2024-05-31 14:11:26 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import * as React from 'react';
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="top-300 text-3xl text-center">
-          Hello World!
-              </p>
-              <p className="top-300 text-3xl text-center">
-                  plswork
-              </p>
-      </div>
-    </main>
-  );
-}
+import React from 'react';
+import Layout from './layout';
+
+const HomePage = () => {
+    return (
+        <Layout>
+            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                <p className="top-300 text-3xl text-center">
+                    Hello World!
+                </p>
+            </div>
+        </Layout>
+    );
+};
+
+export default HomePage;
