d36172c45745267e98cce6a5707f659a7a5cdb64
Hassan Mahmoud <2065740mahmoud@zuyd.nl>
2024-06-26 18:31:00 +0200

    Patient model, patient page toegevoegd

---
+++ models/Patient.ts
@@ -0,0 +1,13 @@
+export interface Patient {
+id: number;
+name: string;
+surname: string;
+age: number;
+gender: string;
+address: string;
+city: string;
+email: string;
+diagnosis: string;
+medication: string;
+phonenumber: number;
+}
