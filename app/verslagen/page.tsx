072d92e8d175aedc305ffe2fa0c170793d82c97a
Tristan Loo <2203939loo@zuyd.nl>
2024-07-01 14:07:16 +0200

    Verslagen Page vrijwel af

--- app/verslagen/page.tsx
+++ app/verslagen/page.tsx
@@ -1,15 +1,17 @@
-"use client";  // Ensure this directive is at the top of the file
+// Ensure this directive is at the top of the file
+"use client";
 
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Verslag } from '../Models/Verslag';
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Input } from "@nextui-org/react"; // Nieuwe import van de tabelcomponenten
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Input, CircularProgress, Button } from "@nextui-org/react";
 
 const VerslagenPage = () => {
     const [verslagen, setVerslagen] = useState<Verslag[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
     const [searchDate, setSearchDate] = useState("");
+    const [creatingNewVerslag, setCreatingNewVerslag] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -34,6 +36,15 @@ const VerslagenPage = () => {
         fetchData();
     }, []);
 
+    const handleCreateNewVerslagClick = () => {
+        setCreatingNewVerslag(true);
+        // Simulate asynchronous creation process
+        setTimeout(() => {
+            // Reset state and perform any necessary actions after creating new verslag
+            setCreatingNewVerslag(false);
+        }, 4000); // Example: Simulate 2 seconds loading time
+    };
+
     if (loading) {
         return <div>Loading...</div>;
     }
@@ -47,15 +58,24 @@ const VerslagenPage = () => {
     );
 
     return (
-        <div style={{ padding: '20px' }}>
-            <h1 style={{ fontSize: 'xx-large', fontWeight: 'bold', borderBottom: '2px solid black', display: 'inline-block', marginBottom: '20px' }}>Verslagen</h1>
-            <Input
-                placeholder="Zoeken op datum..."
-                value={searchDate}
-                onChange={(e) => setSearchDate(e.target.value)}
-                style={{ marginBottom: '10px' }}  // Voeg hier margin toe naar wens
-            />
-            <Table removeWrapper aria-label="Lijst van verslagen" style={{ marginBottom: '20px' }}>
+        <div style={{ padding: '50px', backgroundColor: 'rgb(216, 234, 255)', minHeight: '100vh' }}>
+            <h1 style={{ fontSize: 'xx-large', fontWeight: 'bold', borderBottom: '2px solid black', display: 'inline-block', marginBottom: '50px' }}>Verslagen</h1>
+            <div style={{ display: 'flex', marginBottom: '10px', width: '300px', border: '1px solid transparent', borderRadius: '4px', paddingLeft: '10px', alignItems: 'center' }}>
+                <img src="/searchiconVerslagen.png" alt="Search" style={{ width: '20px', height: '20px', marginRight: '10px' }} />
+                <Input
+                    placeholder="Zoeken op datum..."
+                    value={searchDate}
+                    onChange={(e) => setSearchDate(e.target.value)}
+                    style={{
+                        flex: '1',
+                        background: 'none',
+                        border: 'none',
+                        outline: 'none',
+                    }}
+                />
+            </div>
+
+            <Table isStriped aria-label="Lijst van verslagen" style={{ marginBottom: '20px' }}>
                 <TableHeader>
                     <TableColumn>Datum</TableColumn>
                     <TableColumn>Klachten</TableColumn>
@@ -82,10 +102,37 @@ const VerslagenPage = () => {
                     ))}
                 </TableBody>
             </Table>
-            <div>
-                <Link href='../verslagen/createverslag'>
-                    <button style={{ backgroundColor: 'lightgreen' }}>Maak een nieuw verslag</button>
-                </Link>
+            <div style={{ marginBottom: '20px', marginTop: '20px' }}>
+                {creatingNewVerslag ? (
+                    <CircularProgress aria-label="Loading..." />
+                ) : (
+                    <Link href='../verslagen/createverslag'>
+                        <Button
+                            onClick={handleCreateNewVerslagClick}
+                            radius="full"
+                            style={{
+                                backgroundColor: '#000369', // Achtergrondkleur
+                                cursor: 'pointer',
+                                backgroundImage: `url("/addiconVerslagen.png")`, // Achtergrondafbeelding
+                                backgroundRepeat: 'no-repeat',
+                                backgroundPosition: 'left', // Positie van het icoon
+                                backgroundSize: '25px', // Grootte van het icoon
+                                paddingLeft: '20px', // Ruimte toevoegen voor het icoon
+                                paddingRight: '20px', // Ruimte rechts van de tekst
+                                marginLeft: '10px',
+                                border: 'none',
+                                borderRadius: '4px',
+                                padding: '10px',
+                                fontSize: '16px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'space-between', // Ruimte tussen icoon en tekst
+                            }}
+                        >
+                            <span style={{ marginLeft: '10px', color: "white" }}>Maak een nieuw verslag</span> {/* Extra ruimte voor tekst aan de linkerkant */}
+                        </Button>
+                    </Link>
+                )}
             </div>
         </div>
     );
