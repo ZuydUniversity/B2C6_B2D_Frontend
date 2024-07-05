92c5a6e2f456da3d44aa31935c4b216a0506300d
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-07-05 07:13:20 -0400

    fix env var?

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-import path from 'path'
-import dotenv from 'dotenv'
-
-// Path to your .env.local file
-
-// Check if BACKEND_BASE_URL is already set
-if (!process.env.BACKEND_BASE_URL) {
-    const envLocalPath = path.resolve(process.cwd(), '.env.local');
-
-    console.log(`[*] loading env vars from: ${envLocalPath}`)
-
-    dotenv.config({ path: envLocalPath });
-
-    console.log(`[*] BACKEND_BASE_URL: ${process.env.BACKEND_BASE_URL}`)
-}
-
 const nextConfig = {
     output: 'standalone',  /* build executable next.js webserver (instead of only static files) */
-    env: {
-        BACKEND_BASE_URL: process.env.BACKEND_BASE_URL
-    }
 };
 
 
