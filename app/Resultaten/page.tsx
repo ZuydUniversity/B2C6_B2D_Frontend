8cd1654a91f8d255d38f60bf69e9606ca348e65c
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-07-05 09:09:18 -0400

    fix: stuff

--- app/Resultaten/page.tsx
+++ app/Resultaten/page.tsx
@@ -6,37 +6,18 @@ import { Resultaat } from '../models/Resultaat';
 export default function GetResultaten() {
   const [data, setData] = useState<Resultaat[]>([]);
 
-  useEffect(() => {
+  function fetchData() {
     // Replace with your API endpoint
     fetch('/api/resultaten/')
       .then(response => response.json())
       .then((data: Resultaat[]) => setData(data))
       .catch(error => console.error('Error fetching data:', error));
+  }
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
-  return (
-    <div>
-      <h1>Data Table</h1>
-      <table border={1}>
-        <thead>
-          <tr>
-            {data.length > 0 && Object.keys(data[0]).map((key) => (
-              <th key={key}>{key}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              {Object.values(item).map((value, i) => (
-                    <td key={i}><a href={`/api/resultaten/${item['id']}`}>{value}</a></td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
     const renderCellValue = (value: any) => {
         if (typeof value === 'object' && value !== null) {
             // If the value is an object, convert it to a string representation
