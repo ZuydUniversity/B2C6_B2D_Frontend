300a7358184defdf44b155c5135ab55d4c4aaec6
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-07-05 06:50:10 -0400

    try updating env

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Path to your .env.local file
+
+// Check if BACKEND_BASE_URL is already set
+if (!process.env.BACKEND_BASE_URL) {
+    const envLocalPath = path.resolve(process.cwd(), '.env.local');
+
+    console.log("[*] loading env vars")
+
+    dotenv.config({ path: envLocalPath });
+}
+
 const nextConfig = {
     output: 'standalone'  /* build executable next.js webserver (instead of only static files) */
 };
-
 
 
 export default nextConfig;
