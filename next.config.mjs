9a4ba9af945d41494309dd1474e9ea0da163c928
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-07-05 07:28:06 -0400

    fix env var config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 
 const nextConfig = {
     output: 'standalone',  /* build executable next.js webserver (instead of only static files) */
+    env: {
+        NEXT_PUBLIC_BACKEND_BASE_URL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+    },
 };
 
 
