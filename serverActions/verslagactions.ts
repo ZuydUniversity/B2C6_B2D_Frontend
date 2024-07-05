85436e9da735eb5fd31cafea1d6d88cd6a9fb200
PieDePopper <2200279ogg@zuyd.nl>
2024-07-05 20:30:16 +0200

    .

--- serverActions/verslagactions.ts
+++ serverActions/verslagactions.ts
@@ -67,4 +67,25 @@ export const deleteVerslag = async (id: string) => {
     }
 };
 
+export const updateVerslag = async (id: string, verslag: any) => {
+    try {
+        const response = await fetch(`http://127.0.0.1:8000/verslag/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(verslag),
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to update verslag');
+        }
+
+        return response.json();
+    } catch (error) {
+        console.error(`Error updating verslag with id ${id}:`, error);
+        throw error; // Rethrow de fout om deze door te geven aan de caller
+    }
+};
+
 
